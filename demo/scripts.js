3138882f3b8997c6a947c938eaaa4bae17e1f0c4
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-06-01 17:59:23 -1000

    edit: removes loop from convert to transfrom

--- demo/scripts.js
+++ demo/scripts.js
@@ -42,19 +42,40 @@ scrollElements.forEach(el => {
 });
 
 // Chain
+// let from = { opacity: 0, color: 'blue', transformOrigin: 'center', borderRadius: '5px', x: '20px', y: '50px' };
+// let to = { opacity: 1, color: 'red', borderRadius: '10%', x: '0px', y: '0px' };
+
+
+
 chain([
     {
         element: '.first',
         duration: 0.4,
-        delay: 0.2
+        delay: 0.2,
+        from: {
+            opacity: 0
+        },
+        to: {
+            opacity: 1
+        },
     },
     {
         element: '.second',
         duration: 1,
+        from: {
+            opacity: 0,
+            x: '500px'
+        },
+        to: {
+            opacity: 1,
+            x: 0
+        }
     },
     {
         element: '.third',
-        duration: 0.5,
+        duration: 0.7,
+        from: { scale: 1 },
+        to: { scale: 2 }
     }
 ])
 
--- src/chain.js
+++ src/chain.js
@@ -5,28 +5,51 @@ function toKebabCase(str) {
 // Takes shortcut props and converts them to css transforms
 function convertToTransform(prop) {
     let newStyles;
-    let newAnimValue;
+    let newAnimValue = [];
 
     const elementAnimProps = Object.keys(prop);
     const values = ['x', 'y', 'z', 'rotate', 'rotateX', 'rotateY', 'rotateZ', 'scale', 'scaleX', 'scaleY', 'scaleZ']
+    const animatable = elementAnimProps.filter(animProp => values.includes(animProp));
 
-    values.map((value, i) => {
-        const valid = elementAnimProps.filter(animProp => animProp === value);
 
-        // Handles all property edge cases
-        if (valid[0] === 'x' || 'y' || 'z') {
-            const { x = 0, y = 0, z = 0 } = prop;
-            newAnimValue = `translate3d(${x}, ${y}, ${z})`;
-        }
+    // Handles all property edge cases
+    if (animatable.includes('x' || 'y' || 'z')) {
+        const { x = 0, y = 0, z = 0 } = prop;
+        newAnimValue = [...newAnimValue, `translate3d(${x}, ${y}, ${z}) `];
+    }
 
-    });
+    if (animatable.includes('scale')) {
+        const { scale } = prop;
+        newAnimValue = [...newAnimValue, `scale(${scale}) `]
+    }
 
-    newStyles = { ...prop, transform: newAnimValue };
+    // values.map((value, i) => {
+    //     const valid = elementAnimProps.filter(animProp => animProp === value);
+
+
+
+    //     // Handles all property edge cases
+    //     if (valid[0] === 'x' || 'y' || 'z') {
+    //         const { x = 0, y = 0, z = 0 } = prop;
+    //         newAnimValue = [...newAnimValue, `translate3d(${x}, ${y}, ${z}) `];
+    //     }
+
+    //     if (valid[0] === 'scale') {
+    //         const { scale } = prop;
+    //         newAnimValue = [...newAnimValue, `scale(${scale}) `]
+    //     }
+
+
+    // });
+
+
+
+    newStyles = { ...prop, transform: newAnimValue.join('') };
     return newStyles
 }
 
 // Create a new css class for each element with their animations
-function createDynamicStyle(el, from, to, duration, delay) {
+function createDynamicStyle(el, index, from, to, duration, delay) {
     let elementStyle = document.createElement('style');
     document.head.appendChild(elementStyle);
 
@@ -40,7 +63,7 @@ function createDynamicStyle(el, from, to, duration, delay) {
     Object.entries(convertedFrom).map(el => { fromStyles += `${toKebabCase(el[0])}:${el[1]};` });
     Object.entries(convertedTo).map(el => { toStyles += `${toKebabCase(el[0])}:${el[1]};` });
 
-    let keyframe = `@keyframes elementAnimation{
+    let keyframe = `@keyframes element${index}Animation{
         from{
             ${fromStyles}
         }
@@ -51,23 +74,20 @@ function createDynamicStyle(el, from, to, duration, delay) {
 
     // Inject new styles into the css 
     elementStyle.sheet.insertRule(keyframe);
-    elementStyle.sheet.insertRule(`${el} { ${fromStyles}; animation: elementAnimation  ${duration}s forwards ${delay} }`);
+    elementStyle.sheet.insertRule(`${el} { ${fromStyles}; animation: element${index}Animation  ${duration}s forwards ${delay} }`);
 }
 
 // Main chain function
 export default function chain(elementsArr) {
     elementsArr.map((el, i) => {
-        const { element, delay = 0, duration = 1 } = el;
+        const { element, delay = 0, from, to, duration = 1 } = el;
 
         let elementDelay = `${delay}s`;
-
-        let from = { opacity: 0, color: 'blue', transformOrigin: 'center', borderRadius: '5px', x: '20px', y: '50px' };
-        let to = { opacity: 1, color: 'red', borderRadius: '10%', x: '0px', y: '0px' };
 
         if (i > 0) {
             elementDelay = `${elementsArr[i - 1].duration + delay}s`
         }
 
-        createDynamicStyle(element, from, to, duration, elementDelay)
+        createDynamicStyle(element, i, from, to, duration, elementDelay)
     })
 }
