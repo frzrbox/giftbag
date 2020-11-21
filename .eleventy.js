fb99a6f4f100b6c46e5d03a17ce7f64995911234
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-11-20 19:14:34 -1000

    feat: adds minified css critical styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 // Transforms
 const htmlMinTransform = require('./site/transforms/html-min-transform.js');
+const CleanCSS = require('clean-css');
 
 // Create a helpful production flag
 const isProduction = process.env.NODE_ENV === 'production';
@@ -8,6 +9,10 @@ module.exports = (config) => {
 	if (isProduction) {
 		config.addTransform('htmlmin', htmlMinTransform);
 	}
+
+	config.addFilter('cssmin', function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 
 	config.addPassthroughCopy('./src/assets/');
 
